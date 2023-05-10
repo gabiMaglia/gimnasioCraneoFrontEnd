244e50528e3f47f26825ac62f41e0e3d928d0723
atlasAir <gab.maglia@gmail.com>
2023-05-09 23:58:55 -0300

    reload

--- src/components/common/docente-card/DocenteCard.jsx
+++ src/components/common/docente-card/DocenteCard.jsx
@@ -5,37 +5,36 @@ const docenteCard = (props) => {
 
   return (
     <div className="card-cont">
-        <figure className="card">
-            <div className="title">
-                <div className="text">
-                      <h2>
-                        {nombre} {apellido}
-                      </h2>
-                      <p>{titulo}</p>
-                </div>
-            </div>
-            
-            <img src={foto} alt={apellido} className="picture" />
-                
-                
-           
-           <div className="socialStuff">
-                    <a
-                      href={"http://www.facebook.com/" + facebook}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className="icon-facebook"></i>
-                    </a>
-                    <a
-                      href={"http://www.instagram.com/" + instagram}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className="icon-instagram"></i>
-                    </a>
-           </div>
-        </figure>
+      <figure className="card">
+        <div className="title">
+          <div className="text">
+            <h2>
+              {nombre} {apellido}
+            </h2>
+            <p>{titulo}</p>
+          </div>
+        </div>
+        <div className="picCont">
+          <img src={foto} alt={apellido} className="picture" />
+        </div>
+
+        <div className="socialStuff">
+          <a
+            href={"http://www.facebook.com/" + facebook}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="icon-facebook"></i>
+          </a>
+          <a
+            href={"http://www.instagram.com/" + instagram}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="icon-instagram"></i>
+          </a>
+        </div>
+      </figure>
     </div>
   );
 };
--- src/components/common/mapaGoogle/MapaGoogle.jsx
+++ src/components/common/mapaGoogle/MapaGoogle.jsx
@@ -1,39 +1,16 @@
 import React from "react";
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 
 const MapaGoogle = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.GOOGLE_MAP_API_KEY,
-  });
-
-  function Map() {
-    return (
-      <GoogleMap
-        mapContainerStyle={{
-          width: "100%",
-          height: "300px",
-        }}
-        zoom={15}
-        center={{
-          lat: -32.857914925485666,
-          lng: -60.69858172883605,
-        }}
-
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      >
-        <MarkerF
-          position={{ lat:-32.857889730489106, lng: -60.6985559645807 }}
-        />
-
-      </GoogleMap>
-    );
-  }
-
-  if (!isLoaded) return <div></div>;
   return (
-    <Map />
-    
+    <iframe
+      title="craneo"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1675.7603637678028!2d-60.69931772365907!3d-32.857939297659485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b653dfd478e863%3A0x32a751dfff8905f6!2sJ.%20M.%20Estrada%20327%2C%20S2152BIG%20Granadero%20Baigorria%2C%20Santa%20Fe!5e0!3m2!1ses!2sar!4v1683424117801!5m2!1ses!2sar"
+      width="100%"
+      height="400"
+      allowfullscreen="true"
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
   );
 };
 
