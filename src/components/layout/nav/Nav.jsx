0a25e4c9a68086d867ef8f3dbbce68b25c955d72
atlasAir <gab.maglia@gmail.com>
2023-05-14 00:01:00 -0300

    finishedv1toproduction

--- src/components/layout/nav/Nav.jsx
+++ src/components/layout/nav/Nav.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import "./nav.css";
@@ -14,80 +13,54 @@ function NavBar(props) {
   };
 
   return (
-    <nav
-      id="nav-bar"
-      className="navbar navbar-expand-xl navbar-dark bg-dark fixed-top navbar-scroll"
+    <Navbar
+      expand="xl"
+      bg="dark"
+      variant="dark"
+      fixed="top"
+      style={{ backgroundColor: "rgba(0,0,0,0.3)" }}
     >
-      <div className="holder container">
-        <div className="banner ms-5" title="Gimnasio Craneo">
-          <span className="mainLogo">GIMNASIO CRANEO</span>
-        </div>
-        <button
-          className="navbar-toggler btn btn-secondary me-3"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navBarMenu"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i className="fas fa-bars"></i>
-        </button>
-        <Navbar.Collapse
-          className="collapse navbar-collapse me-5"
-          id="navBarMenu"
-        >
-          <Nav className="navbar-nav">
-            <li className="nav-item">
-              <Nav.Link href="#homepage" className="button" title="Inicio">
-                INICIO
-              </Nav.Link>
-            </li>
-            <li className="nav-item">
-              <Nav.Link href="#nosotros" className="button" title="Nosotros">
-                NOSOTROS
-              </Nav.Link>
-            </li>
-            <li className="nav-item">
-              <Nav.Link
-                href="#actividades"
-                className="button"
-                title="Que hacemos"
-              >
-                QUE HACEMOS
-              </Nav.Link>
-            </li>
-            <li className="nav-item">
-              <Nav.Link href="#contacto" className="button" title="Contacto">
-                CONTACO
-              </Nav.Link>
-            </li>
-            <li className="nav-item">
-              <i
-                onClick={() => {
-                  openModal();
-                }}
-                className="button logBtn"
-                title="Login"
-              >
-                LOGIN
-              </i>
-            </li>
-            <Nav.Link
-              className="socialMedia button "
-              href="http://www.instagram.com/Gimnasio_Craneo"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="Instagram"
-            >
-              <i className="icon-instagramnormal "></i>
-            </Nav.Link>
-          </Nav>
+      <Navbar.Brand className="banner" title="Gimnasio Craneo">
+        <span className="mainLogo">GIMNASIO CRANEO</span>
+      </Navbar.Brand>
+      <Navbar.Toggle className="toogle" aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav ">
+        <Nav className="ml-auto container">
+          <Nav.Link href="#homepage" className="button" title="Inicio">
+            INICIO
+          </Nav.Link>
+          <Nav.Link href="#nosotros" className="button" title="Nosotros">
+            NOSOTROS
+          </Nav.Link>
+          <Nav.Link href="#actividades" className="button" title="Que hacemos">
+            QUE HACEMOS
+          </Nav.Link>
+          <Nav.Link href="#contacto" className="button" title="Contacto">
+            CONTACO
+          </Nav.Link>
+          <Nav.Link
+            onClick={() => {
+              openModal();
+            }}
+            className="button logBtn"
+            title="Login"
+          >
+            LOGIN
+          </Nav.Link>
 
-          <ModalLogIn show={showModal} setShow={setShowModal} />
-        </Navbar.Collapse>
-      </div>
-    </nav>
+          <Nav.Link
+            className="socialMedia button ms-auto "
+            href="http://www.instagram.com/Gimnasio_Craneo"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Instagram"
+          >
+            <i className="icon-instagramnormal "></i>
+          </Nav.Link>
+        </Nav>
+        <ModalLogIn show={showModal} setShow={setShowModal} />
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
